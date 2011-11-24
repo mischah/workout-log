3fe26c02189bc46e5beb5dff80e2f2c88f5482c8
Michael Kühnel <mail@michael-kuehnel.de>
2011-11-24 21:51:49 +0100

    [+FEATURE] Add Xcode project.

---
+++ xcode/Workout Log/Workout Log/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Workout Log
+//
+//  Created by Michael Kühnel on 23.11.11.
+//  Copyright SMA Solar Technology AG 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
