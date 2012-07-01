83976a5c9b7ad32dee7f809075c290e22ad4db79
Michael Kühnel <mail@michael-kuehnel.de>
2012-07-01 15:14:30 +0200

    Rename some variables.

--- Titanium/Resources/ui/ApplicationTabGroup.js
+++ Titanium/Resources/ui/ApplicationTabGroup.js
@@ -3,34 +3,34 @@ function ApplicationTabGroup(workOutWindow, plansWindow, settingsWindow) {
 	var self = Ti.UI.createTabGroup();
 	
 	//create app tabs
-	var win1 = new workOutWindow(),
-		win2 = new plansWindow(),
-		win3 = new settingsWindow();
+	var workOutWindow = new workOutWindow(),
+		plansWindow = new plansWindow(),
+		settingsWindow = new settingsWindow();
 	
-	var tab1 = Ti.UI.createTab({
+	var workOutTab = Ti.UI.createTab({
 		title: L('workOut'),
 		icon: '/images/nav_workout.png',
-		window: win1
+		window: workOutWindow
 	});
-	win1.containingTab = tab1;
+	workOutWindow.containingTab = workOutTab;
 	
-	var tab2 = Ti.UI.createTab({
+	var plansTab = Ti.UI.createTab({
 		title: L('plans'),
 		icon: '/images/nav_plans.png',
-		window: win2
+		window: plansWindow
 	});
-	win2.containingTab = tab2;
+	plansWindow.containingTab = plansTab;
 	
-	var tab3 = Ti.UI.createTab({
+	var settingsTab = Ti.UI.createTab({
 		title: L('settings'),
 		icon: '/images/nav_settings.png',
-		window: win3
+		window: settingsWindow
 	});
-	win3.containingTab = tab3;
+	settingsWindow.containingTab = settingsTab;
 	
-	self.addTab(tab1);
-	self.addTab(tab2);
-	self.addTab(tab3);
+	self.addTab(workOutTab);
+	self.addTab(plansTab);
+	self.addTab(settingsTab);
 	
 	return self;
 };
