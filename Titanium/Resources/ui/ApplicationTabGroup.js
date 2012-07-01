1ee6b9d28c3a3b2933e5be3b7690276ae4e07101
Michael Kühnel <mail@michael-kuehnel.de>
2012-07-01 11:15:16 +0200

    [+TASK] Add third tab

--- Titanium/Resources/ui/ApplicationTabGroup.js
+++ Titanium/Resources/ui/ApplicationTabGroup.js
@@ -3,25 +3,34 @@ function ApplicationTabGroup(Window) {
 	var self = Ti.UI.createTabGroup();
 	
 	//create app tabs
-	var win1 = new Window(L('home')),
-		win2 = new Window(L('settings'));
+	var win1 = new Window(L('Work out')),
+		win2 = new Window(L('Workout plans')),
+		win3 = new Window(L('Settings'));
 	
 	var tab1 = Ti.UI.createTab({
-		title: L('home'),
+		title: L('Work out'),
 		icon: '/images/KS_nav_ui.png',
 		window: win1
 	});
 	win1.containingTab = tab1;
 	
 	var tab2 = Ti.UI.createTab({
-		title: L('settings'),
+		title: L('Workout plans'),
 		icon: '/images/KS_nav_views.png',
 		window: win2
 	});
 	win2.containingTab = tab2;
 	
+	var tab3 = Ti.UI.createTab({
+		title: L('Settings'),
+		icon: '/images/KS_nav_views.png',
+		window: win3
+	});
+	win3.containingTab = tab3;
+	
 	self.addTab(tab1);
 	self.addTab(tab2);
+	self.addTab(tab3);
 	
 	return self;
 };
