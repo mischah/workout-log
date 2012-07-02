a2935a24a59d8c42e6c3c986b55e2dec445edafc
Michael Kühnel <mail@michael-kuehnel.de>
2012-07-02 12:45:11 +0200

    [TASK] Change properties of detailview.

--- Titanium/Resources/ui/defaults/defaultView.js
+++ Titanium/Resources/ui/defaults/defaultView.js
@@ -1,10 +1,12 @@
 function renderDefaultView() {
 	
 	var defaultView = Titanium.UI.createView({
-	   borderRadius: 10,
+	   borderRadius: 8,
 	   backgroundColor: '#eeb7b7',
-	   width: 300,
-	   height: 346
+	   top: 8,
+	   right: 8,
+	   bottom: 8,
+	   left: 8
 	});
 	
 	return defaultView;
