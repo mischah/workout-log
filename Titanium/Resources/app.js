be7c3011fcfc90e8fe30e1d514c97962ba2502a8
Michael Kühnel <mail@michael-kuehnel.de>
2012-07-03 21:46:14 +0200

    [TASK] First run of JSHint on that file ; ]

--- Titanium/Resources/app.js
+++ Titanium/Resources/app.js
@@ -1,22 +1,22 @@
 /*
- * A tabbed application, consisting of multiple stacks of windows associated with tabs in a tab group.  
- * A starting point for tab-based application with multiple top-level windows. 
+ * A tabbed application, consisting of multiple stacks of windows associated with tabs in a tab group.
+ * A starting point for tab-based application with multiple top-level windows.
  * Requires Titanium Mobile SDK 1.8.0+.
- * 
+ *
  * In app.js, we generally take care of a few things:
  * - Bootstrap the application with any data we need
  * - Check for dependencies like device type, platform version or network connection
  * - Require and open our top-level UI component
- *  
+ *
  */
 
 // This is a single context application with mutliple windows in a stack
 (function() {
 	
 	// opaque black status bar
-	Ti.UI.iPhone.statusBarStyle = Ti.UI.iPhone.StatusBar.OPAQUE_BLACK
+	Ti.UI.iPhone.statusBarStyle = Ti.UI.iPhone.StatusBar.OPAQUE_BLACK;
 	
-	// Require the windows for the tabs 	
+	// Require the windows for the tabs
 	var workOutWindow = require('ui/workOutWindow'),
 	plansWindow = require('ui/plansWindow'),
 	settingsWindow = require('ui/settingsWindow');
